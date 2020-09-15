97c5db2098b92d59f0becd423e84ee57d0c4ed5d
Lutz Krueger <lutz.krueger@veolia.ccom>
2020-09-15 12:39:18 +0200

    Consumer erweitert auf Verarbeitung einer Nachricht

--- src/ConsumerAcked.php
+++ src/ConsumerAcked.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace WAG\RabbitMq;
 
+use PhpAmqpLib\Message\AMQPMessage;
+
 final class ConsumerAcked
 {
     use ExchangeDeclaration;
@@ -13,6 +15,15 @@ final class ConsumerAcked
     public function setCallback(\Closure $callback) : void
     {
         $this->callback = $callback;
+    }
+
+    public function setPrefetchMessages(int $count) : void
+    {
+        $this->channel->basic_qos(
+            0,
+            $count,
+            false
+        );
     }
 
     public function consume() : void
@@ -32,5 +43,36 @@ final class ConsumerAcked
         while ($this->channel->is_consuming()) {
             $this->channel->wait();
         }
+    }
+
+    public function acknowledgesMessage(AMQPMessage $message) : void
+    {
+        $message->get('channel')->basic_ack($message->get('delivery_tag'));
+    }
+
+    public function fetchMessage(string $queueName, bool $doAck = true) : ?AMQPMessage
+    {
+        if (! isset($this->callback)) {
+            throw new \InvalidArgumentException('Callback not defined.');
+        }
+
+        $message = $this->channel->basic_get($queueName);
+        if ($message === null) {
+            return null;
+        }
+
+        $eventualMessage = ($this->callback)($message);
+
+        if ($eventualMessage !== null) {
+            if ($eventualMessage !== $message) {
+                throw new \LogicException('Callback does not returns same message instance or null as expected.');
+            }
+
+            if ($doAck) {
+                $this->acknowledgesMessage($message);
+            }
+        }
+
+        return $eventualMessage;
     }
 }
