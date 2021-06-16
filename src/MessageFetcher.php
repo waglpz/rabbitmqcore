43e90d235eb77c9211c31e8ee061b015557fcf78
Alexander Hutorezki <olekhy@gmail.com>
2021-06-16 02:08:58 +0200

    chore: Adding traits for simplicity

---
+++ src/MessageFetcher.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WAG\RabbitMq;
+
+trait MessageFetcher
+{
+    /** @var array<string, mixed> */
+    private array $callback;
+    /** @var array<string, Consumer> */
+    private array $consumer;
+    /** @var array<mixed> */
+    private static array $configMqConnection;
+    /** @var array<mixed> */
+    private static array $configQueues;
+
+    public function consumer(string $id): Consumer
+    {
+        if (isset($this->consumer[$id])) {
+            return $this->consumer[$id];
+        }
+
+        if (! isset($this->callback[$id])) {
+            throw new \Error('Callback is not yet defined, must be defined.');
+        }
+
+        $channelBuilder = new ChannelBuilder(
+            self::$configMqConnection['hostname'],
+            self::$configMqConnection['port'],
+            self::$configMqConnection['username'],
+            self::$configMqConnection['password'],
+            self::$configMqConnection['vhost']
+        );
+
+        if (! isset(self::$configQueues[$id])) {
+            throw new \Error('Unbenannte Queue Definition für Id: ' . $id . '.');
+        }
+
+        $channel             = $channelBuilder->channel();
+        $this->consumer[$id] = new Consumer(
+            $channel,
+            self::$configQueues[$id]['exchange']['name'],
+            self::$configQueues[$id]['queues'],
+        );
+
+        $this->consumer[$id]->setCallback($this->callback[$id]);
+
+        return $this->consumer[$id];
+    }
+
+    /** @param mixed $callback
+     */
+    public function onFetchCallback($callback, string $id): void
+    {
+        $this->callback[$id] = $callback;
+    }
+
+    /**
+     * @param mixed[] $configMqConnection
+     * @param mixed[] $configQueues
+     */
+    public function setConfig(array $configMqConnection, array $configQueues): void
+    {
+        self::$configMqConnection = $configMqConnection;
+        self::$configQueues       = $configQueues;
+    }
+}
